6a2b1e4de12c8d31b6e9ca6aba187699020956f7
Tarik Kirgin <hi@tarikkirgin.com>
2024-10-22 22:19:05 +0100

    add manifest v3 support

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Blackboard Native PDF Viewer
 
-Browser extension that reveals Blackboard file link allowing preview in native PDF viewer. For non-PDF files, the file is downloaded. Currently only for Firefox.
+Browser extension that reveals Blackboard file link allowing preview in the native PDF viewer. For non-PDF files, the file is downloaded.
 
 ## Credits
 
--- common/content-script.js
+++ common/content-script.js
@@ -30,9 +30,9 @@ function revealLink(link) {
   link.setAttribute("href", link.getAttribute("data-ally-file-preview-url"));
   link.setAttribute("target", "_blank");
   link.innerHTML = `
-    <svg focusable="false" aria-hidden="true" role="presentation" viewBox="0 0 512 512" style="width: 1em; height: 1em; font-size: 1.25rem; line-height: 1; margin-top: 0.25em; margin-right: 0.75em;">
+    <svg focusable="false" aria-hidden="true" role="presentation" viewBox="0 0 512 512" style="width: 1em; height: 1em; font-size: 1.25rem; line-height: 1; margin-bottom: -0.3em;">
   <!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
-  <path d="M320 0c-17.7 0-32 14.3-32 32s14.3 32 32 32h82.7L201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L448 109.3V192c0 17.7 14.3 32 32 32s32-14.3 32-32V32c0-17.7-14.3-32-32-32H320zM80 32C35.8 32 0 67.8 0 112V432c0 44.2 35.8 80 80 80H400c44.2 0 80-35.8 80-80V320c0-17.7-14.3-32-32-32s-32 14.3-32 32V432c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V112c0-8.8 7.2-16 16-16H192c17.7 0 32-14.3 32-32s-14.3-32-32-32H80z"></path>
+  <path fill="currentColor" d="M320 0c-17.7 0-32 14.3-32 32s14.3 32 32 32h82.7L201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L448 109.3V192c0 17.7 14.3 32 32 32s32-14.3 32-32V32c0-17.7-14.3-32-32-32H320zM80 32C35.8 32 0 67.8 0 112V432c0 44.2 35.8 80 80 80H400c44.2 0 80-35.8 80-80V320c0-17.7-14.3-32-32-32s-32 14.3-32 32V432c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V112c0-8.8 7.2-16 16-16H192c17.7 0 32-14.3 32-32s-14.3-32-32-32H80z"></path>
   </svg>
     `;
 }
